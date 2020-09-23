a745e35dc7981093796b32616e2703983839e1e2
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-09-23 01:48:37 -0400

    added files

---
+++ shapes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "shapes.h"
+
+//This contain task D, E, F
+
+//Task D
+std::string lower (int length) {
+  
+  std::string result = "Bottom-left half of a square of length "+ std::to_string(length) + ":\n";
+
+  
+  return result;
+}
